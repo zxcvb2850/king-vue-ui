d09e50199744c70f00148043544ba426ad65ec1e
wangrui <wangrui1732@stnts.com>
2019-06-05 16:42:23 +0800

    feat: 全局alert组件

---
+++ src/components/alert/alert.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="alert">
+      <div v-for="item in notices" :key="item.name">
+        <div class="alert-content">{{item.content}}</div>
+      </div>
+    </div>
+</template>
+
+<script>
+let seed = 0;
+
+function getUid() {
+  // eslint-disable-next-line no-plusplus
+  return `alert_${seed++}`;
+}
+export default {
+  name: 'kAlert',
+  data() {
+    return {
+      notices: [],
+    };
+  },
+  methods: {
+    add(notice) {
+      const name = getUid();
+
+      // eslint-disable-next-line no-underscore-dangle
+      const _notice = Object.assign({
+        name,
+      }, notice);
+
+      this.notices.push(_notice);
+
+      // 定时移除， 单位：秒
+      const { duration } = notice;
+      setTimeout(() => {
+        this.remove(name);
+      }, duration * 1000);
+    },
+    remove(name) {
+      const { notices } = this;
+
+      // eslint-disable-next-line no-plusplus
+      for (let i = 0; i < notices.length; i++) {
+        if (notices[i].name === name) {
+          this.notices.splice(i, 1);
+          break;
+        }
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .alert{
+    position: fixed;
+    width: 100%;
+    top: 16px;
+    left: 0;
+    text-align: center;
+    pointer-events: none;
+  }
+  .alert-content{
+    display: inline-block;
+    padding: 8px 16px;
+    background: #fff;
+    border-radius: 3px;
+    box-shadow: 0 1px 6px rgba(0, 0, 0, .2);
+    margin-bottom: 8px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 import router from './router';
+import Alert from './components/alert/alert';
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$Alert = Alert;
 
 new Vue({
   store,
