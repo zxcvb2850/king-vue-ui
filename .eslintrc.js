554405e0cb76f9aaf46f8f2bcbcb2634b7421d0a
zxcvb2850 <zxcvb2850@163.com>
2019-07-08 21:43:49 +0800

    ﻿feat: 添加postcss， eslint配置文件
    添加对CSS的编译，和对代码的格式要求

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,22 @@
 module.exports = {
   root: true,
+  parserOptions: {
+    parser: "babel-eslint",
+  },
   env: {
+    browser: true,
     node: true,
+    es6: true,
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
+    "plugin:vue/essential",
+    "plugin:vue/recommended",
+    "eslint:recommended",
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'import/extensions': 0,
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "import/extensions": 0,
+    "vue/require-default-prop": 0,
   },
 };
