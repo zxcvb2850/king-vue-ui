97ec66bd38efb6a5f2233fcb4555e5fd91941b92
zxcvb2850 <zxcvb2850@163.com>
2019-07-10 12:06:54 +0800

    ﻿test: 新增对alert组件测试
    创建简单的测试

--- test/unit/specs/alert.spec.js
+++ test/unit/specs/alert.spec.js
@@ -1,9 +1,18 @@
 import Vue from "vue";
 import { expect } from "chai";
-import Alert from '../../../src/components/alert';
+import Alert from "../../../src/components/alert";
 
-describe('Alert', () => {
-  it('create', () => {
-    const wrapper = Vue(Alert).$mount()
+describe("Alert", () => {
+  const Ctor = Vue.extend(Alert);
+
+  it("create", () => {
+    const wrapper = new Ctor({
+      propsData: {
+        title: "test",
+        showIcon: true,
+      },
+    }).$mount();
+
+    expect(wrapper.$el.querySelector(".k-alert__title").textContent).to.equal("test");
   });
 });
