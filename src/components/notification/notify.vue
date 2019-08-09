acd3c135e29e4473bb9948908493bf035351f61c
zxcvb2850 <zxcvb2850@163.com>
2019-08-09 10:13:48 +0800

    style: delete .vue file style tag

--- src/components/notification/notify.vue
+++ src/components/notification/notify.vue
@@ -48,23 +48,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-  .alert{
-    position: fixed;
-    width: 100%;
-    top: 16px;
-    left: 0;
-    text-align: center;
-    pointer-events: none;
-    color: #000;
-  }
-  .alert-content{
-    display: inline-block;
-    padding: 8px 16px;
-    background: #fff;
-    border-radius: 3px;
-    box-shadow: 0 1px 6px rgba(0, 0, 0, .2);
-    margin-bottom: 8px;
-  }
-</style>
